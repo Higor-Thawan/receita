c5173218caae9cc732ff5d8e5d6749d7327ca01f
higor <higor.silva@sensedia.com>
2022-04-04 11:48:33 -0300

    first commit

---
+++ src/test/java/com/receitas/culin/rias/ReceitasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.receitas.culin.rias;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReceitasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
